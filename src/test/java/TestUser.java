7d509e77393da2a6f08de2980de7ba1e96818964
DieroKreator <qtechwired@gmail.com>
2024-11-04 17:53:09 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,27 @@
+// 0 - nome do pacote
+
+// 1 - bibliotecas
+import org.junit.jupiter.api.Test;
+
+// 2 - classe
+public class TestUser {
+    // 2.1 atributos
+    static String ct = "application/json";
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+    // função de leitura de Json
+    public static String lerArquivoCSV(String arquivoJson){
+        // TODO: Completar a leitura do arquivo
+        // return new String(Files)
+        return null
+    }
+
+
+    // 2.2 funções e métodos
+    @Test
+    public void testPostUser(){
+        // carregar os dados do arquivo json do pet
+        // TODO: Criar o método Post
+        // String jsonBody = 
+    }
+}
