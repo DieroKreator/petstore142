ec3638ee33effaa3562a2c9816644389ce05a0e3
DieroKreator <qtechwired@gmail.com>
2024-11-06 16:26:24 -0300

    Added test body

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,7 +1,18 @@
 // 0 - nome do pacote
 
 // 1 - bibliotecas
+import io.restassured.response.Response;
+
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.*;
 
 // 2 - classe
 public class TestPet {
@@ -9,19 +20,34 @@ public class TestPet {
     static String ct = "application/json";
     static String uriPet = "https://petstore.swagger.io/v2/pet";
 
+    // 2.2 funções e métodos
+    // 2.2 funções e métodos comuns / uteis
+
     // função de leitura de Json
-    public static String lerArquivoCSV(String arquivoJson){
-        // TODO: Completar a leitura do arquivo
-        // return new String(Files)
-        return null;
+    public static String lerArquivoCSV(String arquivoJson) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(arquivoJson)));
     }
 
 
-    // 2.2 funções e métodos
     @Test
-    public void testPostUser(){
+    public void testPostPet() throws IOException{
         // carregar os dados do arquivo json do pet
-        // TODO: Criar o método Post
-        // String jsonBody = 
+        String jsonBody = lerArquivoCSV("/src/test/resources/json/pet1.json");
+        String petId = "602740501";
+
+        given()
+            .contentType(ct)
+            .log().all()
+            .body(jsonBody)
+        .when()
+            .post(uriPet)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is("Snoopy"))
+            .body("id", is(petId))
+            .body("category.name", is("cachorro"))
+            .body("tags[0].name", is("vacinado"))
+        ;
     }
 }
