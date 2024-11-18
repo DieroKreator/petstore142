7b553ac2aa486c956cc75cfb1129b248b56954e4
DieroKreator <qtechwired@gmail.com>
2024-11-18 09:43:51 -0300

    Improve class Pet with more fileds added

--- src/test/java/Pet.java
+++ src/test/java/Pet.java
@@ -1,7 +1,15 @@
 public class Pet {
-    public int petId;
-    public String petName;
-    public int catId;
-    public String catName;
+    public int id;
+    public class Category {
+        public int id;
+        public String name;
+    } 
+    public String name; 
+    public String [] photoUrls;   
+    public class Tag {
+        public int id;
+        public String name;
+    }  
+    public Tag tags[];
     public String status;
 }
